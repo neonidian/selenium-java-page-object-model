def19f3e47cce818443e78e9798df0d156c2113a
neonidian <neonidian@gmail.com>
2019-07-13 11:30:15 +0200

    Remove unused dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,14 +9,10 @@ repositories {
 }
 
 dependencies {
-    api("org.apache.commons:commons-math3:3.6.+")
-    implementation("com.google.guava:guava:27.0.1-jre")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.4.+")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.4.+")
     testImplementation("org.seleniumhq.selenium:selenium-java:4.0.+")
     testImplementation("io.github.bonigarcia:webdrivermanager:3.6.+")
     testImplementation("org.hamcrest:hamcrest-all:1.3")
-    implementation("commons-configuration:commons-configuration:1.10")
 }
 
 tasks.wrapper {
