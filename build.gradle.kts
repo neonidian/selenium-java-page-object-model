6f457353ae7fcf23bbacbacd31a32a8fe96bf137
neonidian <neonidian@gmail.com>
2019-08-08 21:40:30 +0200

    Upgrade junit version, remove gradle all distribution in wrapper

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,15 +9,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.4.+")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.5.+")
     testImplementation("org.seleniumhq.selenium:selenium-java:4.0.+")
     testImplementation("io.github.bonigarcia:webdrivermanager:3.6.+")
     testImplementation("org.hamcrest:hamcrest-all:1.3")
-}
-
-tasks.wrapper {
-    distributionType = Wrapper.DistributionType.ALL
-    gradleVersion = "5.5.1"
 }
 
 tasks.test {
@@ -25,4 +20,8 @@ tasks.test {
     testLogging {
         events("passed", "skipped", "failed")
     }
+}
+
+tasks.wrapper {
+    gradleVersion = "5.5.1"
 }
